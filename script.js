d8259b112a4b69cac0ae53962135c6dce4b370aa
Fady-24 <u2864233@uel.ac.uk>
2025-05-01 13:41:54 +0300

    slight changes + starting the menu page

--- script.js
+++ script.js
@@ -123,3 +123,17 @@ document.addEventListener('DOMContentLoaded', function () {
         updateStarDisplay();
     }
 });
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
